e08a25aae7c5ebe66f90ed0c901dbbeab9213237
KrishnaKondra18 <101329928+Deeplearner18@users.noreply.github.com>
2024-08-19 18:16:19 -0400

    Removing the last item using pop

--- main.js
+++ main.js
@@ -8,3 +8,8 @@ groceries.prepend("Apple");
 groceries.prepend("Orange");
 console.log("After prepending items:");
 console.log(groceries);
+// 2. Remove the last item using pop
+const removedLast = groceries.pop();
+console.log(`Popped the last item: ${removedLast}`);
+console.log("After popping the last item:");
+console.log(groceries);
