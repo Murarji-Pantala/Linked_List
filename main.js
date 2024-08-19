b168baba54edbdbb117267381f6116fabc67aa7f
KrishnaKondra18 <101329928+Deeplearner18@users.noreply.github.com>
2024-08-19 18:20:48 -0400

    Inserting a item

--- main.js
+++ main.js
@@ -13,3 +13,9 @@ const removedLast = groceries.pop();
 console.log(`Popped the last item: ${removedLast}`);
 console.log("After popping the last item:");
 console.log(groceries);
+
+
+// 3. Insert an item at a specific position
+groceries.insertAt("Grapes", 1); // Inserting at index 1
+console.log('After inserting "Grapes" at index 1:');
+console.log(groceries);
