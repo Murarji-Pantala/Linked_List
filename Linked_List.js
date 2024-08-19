ab9b3ea36d5c4037b2beb2f9c36f3020e4eaa2b8
Kudumula Akshith Yadav <akshithyadav111@gmail.com>
2024-08-18 22:30:36 -0400

    1.added prepend method to add a node in the linkedlist, 2.added pop method to remove last node of the linked list

--- Linked_List.js
+++ Linked_List.js
@@ -10,6 +10,37 @@ class Node {
       this.head = null;
       this.size = 0;
     }
+    prepend(data) {
+      const newNode = new Node(data);
+      if (!this.head) {
+        this.head = newNode;
+      } else {
+        newNode.next = this.head;
+        this.head = newNode;
+      }
+      this.size++;
+    }
+  
+    // Method to remove the last node
+    pop() {
+      if (!this.head) return null;
+  
+      if (this.size === 1) {
+        const data = this.head.data;
+        this.head = null;
+        this.size--;
+        return data;
+      }
+  
+      let current = this.head;
+      while (current.next.next) {
+        current = current.next;
+      }
+      const data = current.next.data;
+      current.next = null;
+      this.size--;
+      return data;
+    }
   }
 
   module.exports = LinkedList;
